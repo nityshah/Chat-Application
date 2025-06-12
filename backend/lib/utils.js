d2c0496a4d41e26f94238c0be144d0c270fedfeb
Shah Nitya <nitya.shah1212@gmail.com>
2025-06-12 15:44:30 +0530

    Fix: correct port usage and cleaned up socket server

--- backend/lib/utils.js
+++ backend/lib/utils.js
@@ -5,12 +5,18 @@ export const generateToken = (userId, res) => {
         expiresIn: "7d"
     })
 
+    // res.cookie("jwt", token, {
+    //     maxAge: 7 * 24 * 60 * 60 * 1000,
+    //     httpOnly: true, // prevent XSS attacks cross-site scripting attacks
+    //     sameSite: "Strict", // CSRF attacks cross-site request forgery attacks
+    //     secure: process.env.NODE_ENV !== "development"
+    // });
     res.cookie("jwt", token, {
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-        httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-        sameSite: "Strict", // CSRF attacks cross-site request forgery attacks
-        secure: process.env.NODE_ENV !== "development"
-    });
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,          // Protects from JavaScript access
+    sameSite: "None",        // Allows cookie to be sent cross-site ✅
+    secure: true             // Cookie only sent over HTTPS ✅
+});
 
     return token;
 }
